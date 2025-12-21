1670054dbc7b0f739577d164f3f5049b1d10ff19
danya <danya>
2025-12-21 23:37:59 +0300

    играюсь с версткой

--- SkillBoxUI_2/SkillBoxUI_2/ViewController.swift
+++ SkillBoxUI_2/SkillBoxUI_2/ViewController.swift
@@ -8,17 +8,20 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
     @IBOutlet weak var displayLabel: UILabel!
-    var typingNumber = false //переменная для проверки вводит пользователь число или он закончил операцию
+    ///переменная для проверки вводит пользователь число или он закончил операцию
+    var typingNumber = false
     var firstNumber: Double = 0
     var secondNumber: Double = 0
-    var operationType: Int = 0 //какой знак вычисления выбрали
-    var dotIsPlace = false //для проверки стоит точка или нет
-    //вычисляемое значение (число, которое на экране) + удаляю с его помощью точки
+    ///какой знак вычисления выбрали
+    var operationType: MathOperation?
+    ///для проверки стоит точка или нет
+    var dotIsPlace = false
+    ///вычисляемое значение (число, которое на экране) + удаляю с его помощью точки
     var currentInput: Double {
         get {
-            return Double(displayLabel.text!)!
+            let text = displayLabel.text ?? "0"
+            return Double(text) ?? 0
         }
         set {
             let value = Int(newValue)
@@ -28,7 +31,7 @@ final class ViewController: UIViewController {
             } else {
                 displayLabel.text = "\(value)"
             }
-
+            
             typingNumber = false
         }
     }
@@ -37,96 +40,146 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .black
     }
-
-
+    
+    //нажатие 0-9
     @IBAction func nubmerPressed(_ sender: UIButton) {
-        let number = sender.tag
+        guard
+            //прикол поделится прикол пофикшен
+            let title = sender.titleLabel?.text,
+            let currentValue = displayLabel.text
+        else { return }
         
-        if typingNumber {
-            displayLabel.text = displayLabel.text! + String(number)
-        } else {
-            displayLabel.text = String(number)
+        // если пользователь только начал ввод
+        guard typingNumber else {
+            displayLabel.text = title
             typingNumber = true
+            return
         }
+        
+        // меняем начальный "0"
+        guard currentValue != "0" else {
+            displayLabel.text = title
+            return
+        }
+        
+        displayLabel.text = currentValue + title
     }
     
+    //нажатие операции
     @IBAction func activeComputing(_ sender: UIButton) {
-        operationType = sender.tag
+        guard
+            let text = sender.titleLabel?.text,
+            let operation = MathOperation(rawValue: text)
+        else { return }
+        
+        operationType = operation
         firstNumber = currentInput
         typingNumber = false
         dotIsPlace = false
-        }
+    }
     
+    //вычисление
     @IBAction func equalityPressed(_ sender: UIButton) {
-        var result: Double = 0
+        guard let operation = operationType else { return }
         
         if typingNumber {
             secondNumber = currentInput
         }
         
+        switch operation{
+        case .add:
+            currentInput = firstNumber + secondNumber
+        case .subtract:
+            currentInput = firstNumber - secondNumber
+        case .multiply:
+            currentInput = firstNumber * secondNumber
+        case .divide:
+            guard secondNumber != 0 else {
+                resetAll()
+                return
+            }
+            currentInput = firstNumber / secondNumber
+        }
+        operationType = nil
         dotIsPlace = false
         
-//эти кейсы для операторов сделал через цифры, чтобы таг вызвать и меньше париться
-        switch operationType {
-        case 1:
-            result = firstNumber + secondNumber
-            typingNumber = false
-        case 2:
-            result = firstNumber - secondNumber
-            typingNumber = false
-        case 3:
-            result =  firstNumber * secondNumber
-            typingNumber = false
-        case 4:
-            result =  firstNumber / secondNumber
-            typingNumber = false
-        default: break
-        }
-        
-        currentInput = result
     }
     
-    @IBAction func deleteLastNumber(_ sender: UIButton) {
-        displayLabel.text = String( displayLabel.text!.dropLast())
-        if displayLabel.text == "" {
-            currentInput = 0
-        }
-    }
-    
-    @IBAction func deleteAllNumber(_ sender: UIButton) {
+    func resetAll() {
         firstNumber = 0
         secondNumber = 0
-        operationType = 0
         currentInput = 0
         displayLabel.text = "0"
         typingNumber = false
         dotIsPlace = false
+        operationType = nil
     }
     
-    @IBAction func percentPressed(_ sender: UIButton) {
-        if firstNumber == 0 {
-            currentInput = currentInput / 100
-        } else {
-            secondNumber = firstNumber * currentInput / 100
+    @IBAction func otherOperation(_ sender: UIButton) {
+        guard
+            let title = sender.titleLabel?.text,
+            let operation = OtherOperation(rawValue: title)
+        else { return }
+        
+        switch operation {
+            
+        case .deleteLastNumber:
+            guard let text = displayLabel.text, !text.isEmpty else { return }
+            displayLabel.text = String(text.dropLast())
+            
+            if displayLabel.text == "" {
+                displayLabel.text = "0"
+                typingNumber = false
+            }
+            
+        case .deleteAllNumber:
+            resetAll()
+            
+        case .percentPressed:
+            if operationType == nil {
+                currentInput = currentInput / 100
+            } else {
+                secondNumber = firstNumber * currentInput / 100
+                currentInput = secondNumber
+            }
             typingNumber = false
+            dotIsPlace = false
+            
+        case .plusAndMinus:
+            currentInput = -currentInput
+            typingNumber = true
+            
+        case .makeDoubleNumber:
+            if typingNumber && !dotIsPlace {
+                displayLabel.text?.append(".")
+                dotIsPlace = true
+                
+            } else if !typingNumber && !dotIsPlace {
+                displayLabel.text = "0."
+                typingNumber = true
+                dotIsPlace = true
+            }
         }
     }
-    
-    @IBAction func plusAndMinus(_ sender: UIButton) {
-        currentInput = -currentInput
-        typingNumber = true
-    }
-    
-    @IBAction func makeDoubleNumber(_ sender: UIButton) {
-        if typingNumber && !dotIsPlace {
-            displayLabel.text = displayLabel.text! + "."
-            dotIsPlace = true
-        } else if !typingNumber && !dotIsPlace {
-            displayLabel.text = "0."
-        }
-    }
-    
 }
 
+
+
+extension ViewController {
+    enum MathOperation: String {
+        case add = "+"
+        case subtract = "-"
+        case multiply = "×"
+        case divide = "÷"
+    }
+    
+    enum OtherOperation: String {
+        case deleteLastNumber = " "
+        case deleteAllNumber = "AC"
+        case percentPressed = "%"
+        case plusAndMinus = "+/-"
+        case makeDoubleNumber = "."
+    }
+}
 
 
