1f959acbaeb8c066e45015b95ac5543bd4b2fc48
danya <danya>
2026-01-06 00:51:10 +0300

    3 задание по UI

---
+++ SkillBoxUI_3/SkillBoxUI_3/WarningViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WarningViewController.swift
+//  SkillBoxUI_3
+//
+//  Created by Danya on 05.01.2026.
+//
+
+import UIKit
+
+final class WarningViewController: UIViewController {
+
+    @IBAction func showAlertButtonTapped(_ sender: UIButton) {
+        showLastScreenAlert()
+    }
+}
+
+
+private extension WarningViewController {
+
+    func showLastScreenAlert() {
+        let alert = UIAlertController(title: "Последний экран", message: "Это последний экран в данной вкладке", preferredStyle: .alert)
+
+        let okAction = UIAlertAction(title: "Ок", style: .default)
+
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
