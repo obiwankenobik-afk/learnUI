c18e28021f8d12a8407ecda47764331290417e1c
danya <danya>
2026-01-09 04:30:58 +0300

    добавил иконки + убрал отступы

--- SkillBoxUI_3/SkillBoxUI_3/WarningViewController.swift
+++ SkillBoxUI_3/SkillBoxUI_3/WarningViewController.swift
@@ -6,17 +6,15 @@
 //
 
 import UIKit
-
 final class WarningViewController: UIViewController {
-
+    
     @IBAction func showAlertButtonTapped(_ sender: UIButton) {
         showLastScreenAlert()
     }
 }
 
-
 private extension WarningViewController {
-
+    
     func showLastScreenAlert() {
         let alert = UIAlertController(title: "Последний экран", message: "Это последний экран в данной вкладке", preferredStyle: .alert)
 
