decc31505ac9b314b0ba26a9ddbb6abf2e7ee081
danya <danya>
2026-01-13 19:33:46 +0300

    исправляем ошибки прошлого

---
+++ SkillBoxUI_2/SkillBoxUI_2/MathOperation.swift
@@ -0,0 +1,13 @@
+//
+//  MathOperation.swift
+//  SkillBoxUI_2
+//
+//  Created by Danya on 13.01.2026.
+//
+
+enum MathOperation: String {
+    case add = "+"
+    case subtract = "-"
+    case multiply = "×"
+    case divide = "÷"
+}
--- SkillBoxUI_2/SkillBoxUI_2/ViewController.swift
+++ SkillBoxUI_2/SkillBoxUI_2/ViewController.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 final class ViewController: UIViewController {
-    //MARK: Переменные для состояния, дисплей, главный экран.
     @IBOutlet weak var displayLabel: UILabel!
-    
-    private var typingNumber = false
     private var firstNumber: Double = 0
-    private var secondNumber: Double = 0
     private var operationType: MathOperation?
-    private var dotIsPlace = false
+    
+    /// Строка, отображаемая в лейбле
+    var displayText: String = "0" {
+        didSet { displayLabel.text = displayText }
+    }
+    
+    /// Числовое значение дисплея
+    var displayValue: Double {
+        get { Double(displayText) ?? 0 }
+        set { formatTextDouble(for: newValue) }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,13 +29,13 @@ final class ViewController: UIViewController {
     }
 }
 
-//MARK: Работа с дисплеем.
+// MARK: - Логика
 private extension ViewController {
-    
-    /// Строка, отображаемая в лейбле
-    var displayText: String {
-        get { displayLabel.text ?? "0" }
-        set { displayLabel.text = newValue }
+    /// проверка на целое число
+    func formatTextDouble (for number: Double) {
+        displayText = number.truncatingRemainder(dividingBy: 1) == 0
+        ? "\(Int(number))"
+        : "\(number)"
     }
     
     func divide() throws {
@@ -119,16 +125,14 @@ private extension ViewController {
     }
 }
 
-//MARK: Взаимодействия с кнопками.
+// MARK: - Взаимодействия с кнопками
 private extension ViewController {
-    
-    ///нажатие 0-9
-    @IBAction func nubmerPressed(_ sender: UIButton) {
+    /// нажатие 0-9
+    @IBAction func numberPressed(_ sender: UIButton) {
         guard let digit = sender.titleLabel?.text else { return }
         
-        if !typingNumber {
+        if Double(displayText) == nil {
             displayText = digit
-            typingNumber = true
             return
         }
         
@@ -139,7 +143,7 @@ private extension ViewController {
         }
     }
     
-    ///нажатие  + - × ÷
+    /// нажатие  + - × ÷
     @IBAction func activeComputing(_ sender: UIButton) {
         guard
             let symbol = sender.titleLabel?.text,
@@ -148,24 +152,18 @@ private extension ViewController {
         
         firstNumber = displayValue
         operationType = operation
-        typingNumber = false
-        dotIsPlace = false
+        displayText = "0"
     }
     
-    ///нажатие =
+    /// нажатие =
     @IBAction func equalityPressed(_ sender: UIButton) {
         guard let operation = operationType else { return }
         
-        if typingNumber {
-            secondNumber = displayValue
-        }
-        
         calculate(operation)
         operationType = nil
-        dotIsPlace = false
     }
     
-    ///нажатие оставшихся операций
+    /// нажатие оставшихся операций
     @IBAction func otherOperation(_ sender: UIButton) {
         guard
             let title = sender.titleLabel?.text,
@@ -175,91 +173,3 @@ private extension ViewController {
         calculateOtherOperation(operation)
     }
 }
-
-//MARK: Логика.
-private extension ViewController {
-    
-    func calculate(_ operation: MathOperation) {
-        switch operation {
-        case .add:
-            displayValue = firstNumber + secondNumber
-        case .subtract:
-            displayValue = firstNumber - secondNumber
-        case .multiply:
-            displayValue = firstNumber * secondNumber
-        case .divide:
-            guard secondNumber != 0 else {
-                resetAll()
-                return
-            }
-            displayValue = firstNumber / secondNumber
-        }
-    }
-    
-    func calculateOtherOperation(_ operation: OtherOperation) {
-        switch operation {
-            
-        case .deleteLastNumber:
-            displayText = String(displayText.dropLast())
-            if displayText.isEmpty {
-                displayText = "0"
-                typingNumber = false
-            }
-            
-        case .deleteAllNumber:
-            resetAll()
-            
-        case .percentPressed:
-            if operationType == nil {
-                displayValue /= 100
-            } else {
-                secondNumber = firstNumber * displayValue / 100
-                displayValue = secondNumber
-            }
-            
-        case .plusAndMinus:
-            displayValue = -displayValue
-            typingNumber = true
-            
-        case .makeDoubleNumber:
-            guard !dotIsPlace else { return }
-            
-            if typingNumber {
-                displayText.append(".")
-            } else {
-                displayText = "0."
-                typingNumber = true
-            }
-            dotIsPlace = true
-        }
-    }
-    
-    func resetAll() {
-        firstNumber = 0
-        secondNumber = 0
-        operationType = nil
-        typingNumber = false
-        dotIsPlace = false
-        displayText = "0"
-    }
-}
-
-//MARK: Перечисления операций.
-extension ViewController {
-    enum MathOperation: String {
-        case add = "+"
-        case subtract = "-"
-        case multiply = "×"
-        case divide = "÷"
-    }
-    
-    enum OtherOperation: String {
-        case deleteLastNumber = " "
-        case deleteAllNumber = "AC"
-        case percentPressed = "%"
-        case plusAndMinus = "+/-"
-        case makeDoubleNumber = "."
-    }
-}
-
-
