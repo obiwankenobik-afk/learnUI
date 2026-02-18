60865adf38af89a1e6201d19c964ee01c89ce493
danya <obiwankenobik@bk.ru>
2026-02-18 17:16:57 +0300

    приколы с инфо исправил + МVP паттерн

--- SkillBoxUI_4/Calculator/Modules/Calculator/CalculatorViewController.swift
+++ SkillBoxUI_4/Calculator/Modules/Calculator/CalculatorViewController.swift
@@ -43,7 +43,6 @@ final class CalculatorViewController: UIViewController {
         setupView()
         setupConstraints()
         makerButtons()
-        
         presenter.viewDidLoad()
     }
 }
